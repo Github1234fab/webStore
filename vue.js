e2adeb7826b6a5bd79caa381b19b961f9900961d
Fabien Marceau <fabien.marceau@gmail.com>
2023-02-02 18:15:09 +0100

    ok affichage BDD

--- vue.js
+++ vue.js
@@ -1,6 +1,33 @@
+const products = [
+    {
+        id: 1, description: "lzcfccfzfczfc", price:25.99 , img: "https://www.yonis-shop.com/169321-amp_product/montre-telephone-android-4g-1-54-1-8go-wifi-noire-bleue.jpg"
+    },
+    {
+        id: 4, description: "lzcfccfzfczfc", price: 25.99, img: "https://boulanger.scene7.com/is/image/Boulanger/6941487257645_h_f_l_0"
+    },
+    {
+        id: 6, description: "2lzcfccfzfczfc", price: 25.99, img: "https://www.cdiscount.com/pdt2/2/1/6/1/700x700/nai5014236277216/rw/naixues-montre-connectee-femme-appels-bluetooth-as.jpg"
+    },
+    {
+        id: 8, description: "lzcfccfzfczfc", price: 25.99, img: "https://www.rueducommerce.fr/media/produits/chrono/img//chronus-montre-connectee-femmes-homme-enfant-ip68-etanche-bracelet-connecte-cran-colore-smartwatch-avec-cardio-podometre-sommeil-reveil-notifications-pour-iphone-huawei-samsung-xiaomi-sonyrouge-10477582-27118438_1140x1140.jpg"
+    },
+    {
+        id: 9, description: "lzcfccfzfczfc", price: 25.99, img: "https://media.carrefour.fr/medias/7fb08c3fda99309e8e5b559f91bc559b/p_540x540/3700092618400-photosite-20211116-121918-0.jpg"
+    },
+    {
+        id: 10, description: "dzczdczdc", price: 23.99, img: "https://media.carrefour.fr/medias/7fb08c3fda99309e8e5b559f91bc559b/p_540x540/3700092618400-photosite-20211116-121918-0.jpg"
+    },
+]
+
+
 const Home = {
-    template: "<h1>Home</h1>",
-    name: "Home"
+    template: "#home",
+    name: "Home",
+    data: () => {
+        return {
+            products,
+        }
+    }
 }
 
 const UserSetting = {
